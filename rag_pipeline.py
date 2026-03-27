a356aaa9ad03942070954fde2814291186c51d81
NaniT55 <20bq1a4255@vvit.net>
2026-03-27 22:26:54 +0530

    Fix deployment huggingface embedding

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -1,6 +1,6 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
 from langchain_openai import ChatOpenAI
 from langchain.prompts import PromptTemplate
